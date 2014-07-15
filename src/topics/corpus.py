745dac98d384f3221a3a0481a9b36120c33c59fd
Fred Lafrance <lafrancef@gmail.com>
2014-07-15 17:35:39 -0400

    Added load method for Corpus objects

--- src/topics/corpus.py
+++ src/topics/corpus.py
@@ -1,5 +1,6 @@
 import re
 from topics_exceptions import ParseException
+from text_document import *
 
 class Corpus:
 	"""
@@ -20,11 +21,11 @@ class Corpus:
 
 		"""
 
+		bypass_init = kwargs.pop("bypass_init", False)
+
 		filter_high = kwargs.pop("filter_high", None)
 		filter_low = kwargs.pop("filter_low", None)
 		filter_set = kwargs.pop("filter_set", None)
-
-		self._docs = docs
 
 		# A[i][j] is the type index for the jth token in the ith document
 		self._types = []
@@ -37,6 +38,14 @@ class Corpus:
 
 		# A[tpe] is the count of type tpe across the corpus
 		self._type_counts = {}
+
+		# A[i] is the document with index i
+		self._docs = []
+
+		if bypass_init:
+			return
+
+		self._docs = docs
 
 		if filter_set is not None:
 			for doc in self._docs:
@@ -103,9 +112,50 @@ class Corpus:
 		for doc in self._docs:
 			yield doc
 
+	def __load_types(self, table):
+		types = table.split(" ")[1:]
+		for idx, tpe in enumerate(types):
+			if len(tpe) == 0:
+				continue
+			self._type_table.append(tpe)
+			self._type_dict[tpe] = idx
+
 	@staticmethod
 	def load(fobj):
-		pass
+		ln = fobj.readline().rstrip()
+		mobj = re.match(r"num_documents: (\d+)", ln)
+		if mobj is None:
+			raise ParseException, "Load corpus: missing number of documents"
+		num_documents = int(mobj.group(1))
+		
+		corpus = Corpus(None, bypass_init=True)
+
+		ln = fobj.readline().rstrip()
+		mobj = re.match(r"type_table:", ln)
+		if mobj is None:
+			raise ParseException, "Load corpus: missing type table"
+
+		corpus.__load_types(ln)
+
+		ln = fobj.readline().rstrip()
+		mobj = re.match(r"documents:", ln)
+		if mobj is None:
+			raise ParseException, "Load corpus: missing documents"
+
+		for i in range(num_documents):
+			doc_words = fobj.readline().rstrip().split(" ")
+			corpus._types.append([])
+			doc = TextDocument("") # TODO Should not necessarily be a text doc.
+			for doc_word in doc_words:
+				corpus._types[i].append(int(doc_word))
+				doc._words.append(corpus._type_table[int(doc_word)])
+				if doc_word in corpus._type_counts:
+					corpus._type_counts[doc_word] += 1
+				else:
+					corpus._type_counts[doc_word] = 1
+			corpus._docs.append(doc)
+
+		return corpus
 
 	def save(self, fobj):
 		fobj.write("num_documents: %d\n" % len(self))
